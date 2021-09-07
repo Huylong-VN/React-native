0e7f388485c408fe78491e62931458859a4de565
Huylong-VN <adultsforfuture@gmail.com>
2021-09-07 13:30:35 +0700

    update router appjs

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {  View } from "react-native";
+import { NativeRouter, Switch, Route } from "react-router-native";
+import Login from './Components/Pages/Login';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+      <NativeRouter>
+          <Switch>
+            <Route exact path="/" component={Login} />
+          </Switch>
+      </NativeRouter>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
