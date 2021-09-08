8670955d2f493eefb77a8a4c9e98e2b8b9d72a49
vanthai3108 <vanthai3108@gmail.com>
2021-09-08 09:45:39 +0700

    edit

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Button, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { NativeRouter, Switch, Route } from "react-router-native";
 
-export default function Home({ children }) {
+
+export default function Layout({children, history }) {
     const [click, setclick] = useState(false);
     const [username, onChangeUsername] = useState('');
     const [password, onChangePassword] = useState('');
 
     return (
         <View style={styles.container}>
-            <View>Home</View>
-            <View>Home</View>
-            <View>Home</View>
+            <View style={styles.header}>Header-search</View>
             {children}
-            <View>Setting</View>
+            <View style={styles.menu}>
+                <View style={styles.menuItem} onPress={() => history.push("/home")}>Home</View>
+                <View style={styles.menuItem} onPress={() => history.push("/home")}>Home</View>
+                <View style={styles.menuItem} onPress={() => history.push("/home")}>Home</View>
+                <View style={styles.menuItem} onPress={() => history.push("/setting")}>Setting</View>
+            </View>
         </View>
     );
 }
@@ -23,4 +28,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    menu: {
+        flexDirection: "row",
+    },
+    menuItem: {
+        flex: 3,
+        height: 50,
+        backgroundColor: "blue",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingLeft: "auto",
+        paddingBottom: "auto",
+    }
 });
