69edcde717f1bbc65de66ec137d138646a9954a8
vanthai3108 <vanthai3108@gmail.com>
2021-09-09 21:02:52 +0700

    edit

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 import { useHistory } from "react-router-native";
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import { color } from 'react-native-elements/dist/helpers';
 
 export default function Layout({ children }) {
     let history = useHistory();
+    const [active, setActive] = useState("home");
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.appName}>News Covid</Text>
+                <TextInput
+                    style={styles.appSearch}
+                    placeholder="Search ..."
+                />
                 <View style={styles.appLink}>
                     <FontAwesome5 style={styles.iconStyles} name={'facebook'} size={25} />
                     <FontAwesome5 style={styles.iconStyles} name={'instagram'} size={25} />
                 </View>
             </View>
-            <Text style={{ marginBottom:"2%"}}></Text>
             <ScrollView>
                 {children}
             </ScrollView>
             <View style={styles.menu}>
-                <TouchableOpacity style={styles.menuItem} onPress={() => history.replace("/home")}>
-                    <FontAwesome5 style={styles.icon} name={'home'} size={25} />
+                <TouchableOpacity style={ active==="home" ? styles.menuItemActive:styles.menuItem } onPress={() => { history.replace("/"); setActive("home") }}>
+                    <FontAwesome5 style={ active==="home" ? styles.active: styles.icon } name={'home'} size={25} />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menuItem} onPress={() => history.replace("/")}>
-                    <FontAwesome5 style={styles.icon}  name={'newspaper'} size={25} />
+                <TouchableOpacity style={ active==="news" ? styles.menuItemActive:styles.menuItem } onPress={() => { history.replace("/setting"); setActive("news") }}>
+                    <FontAwesome5 style={ active==="news" ? styles.active: styles.icon} name={'newspaper'} size={25} />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menuItem} onPress={() => history.replace("/")}>
-                    <FontAwesome5 style={styles.icon}  name={'plus-square'} size={25} />
+                <TouchableOpacity style={ active==="plus" ? styles.menuItemActive:styles.menuItem } onPress={() => { history.replace("/setting"); setActive("plus") }}>
+                    <FontAwesome5 style={ active==="plus" ? styles.active: styles.icon} name={'plus-square'} size={25} />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menuItem} onPress={() => history.replace("/")}>
-                    <FontAwesome5 style={styles.icon}  name={'bell'} size={25} />
+                <TouchableOpacity style={ active==="notify" ? styles.menuItemActive:styles.menuItem }  onPress={() => { history.replace("/setting"); setActive("notify")}}>
+                    <FontAwesome5 style={ active==="notify" ? styles.active: styles.icon} name={'bell'} size={25} />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menuItem} onPress={() => history.replace("/setting")}>
-                    <FontAwesome5 style={styles.icon}  name={'cog'} size={25} />
+                <TouchableOpacity style={ active==="setting" ? styles.menuItemActive:styles.menuItem }  onPress={() => { history.replace("/setting"); setActive("setting") }}>
+                    <FontAwesome5 style={ active==="setting" ? styles.active: styles.icon} name={'cog'} size={25} />
                 </TouchableOpacity>
             </View>
         </View>
@@ -42,63 +45,84 @@ export default function Layout({ children }) {
 
 const styles = StyleSheet.create({
     header: {
-        marginTop: 18,
-        zIndex:1,
+        marginTop: 23,
+        zIndex: 1,
         fontSize: 25,
         fontWeight: "bold",
-        textAlign: "center",
-        backgroundColor: "red",
-        // position:"fixed",
-        top:0,
-        width:"100%",
-        height: "7%",
+        backgroundColor: "#F02849",
+        top: 0,
+        width: "100%",
+        height: "9%",
         flexDirection: "row",
-        alignItems: "center"
+        alignItems: "center",
     },
+
     iconStyles: {
         color: "white",
         margin: 10,
     },
-    appName:{
-        flex: 1,
-        textAlign:"left",
+    appName: {
+        flex: 2,
+        textAlign: "right",
         fontSize: 20,
         fontFamily: "Roboto",
-        fontWeight: "bold"
+        fontWeight: "bold",
+        borderRadius: 10,
+        borderColor: 'black',
     },
-    appLink:{
-        flex: 1,
+    appLink: {
+        flex: 2,
         flexDirection: "row",
-        color:"white",
-        justifyContent:"flex-end"
+        color: "white",
+        justifyContent: "flex-end"
+    },
+    appSearch: {
+        flex: 2,
+        borderRadius: 20,
+        height: "70%",
+        paddingLeft: 10,
+        color: "white"
+        
     },
     container: {
-        flex: 1,
+        flex: 1
     },
     menu: {
         flexDirection: "row",
-        textAlign: "center",
-        alignItems:"center",
+        alignItems: "center",
         width: "100%",
-        backgroundColor:"pink",
-        // position:"fixed",
+        backgroundColor: "rgb(241, 243, 244)",
         bottom: 0,
         height: 49,
-        zIndex:1,
+        zIndex: 1
     },
-    icon:{
-        color: "red",
+    icon: {
+        color: "#F02849",
+    },
+    active: {
+        color: "#1877F2",
     },
     menuItem: {
         flex: 1,
         height: 45,
-        backgroundColor: "yellow",
-        marginRight:5,
-        marginLeft:5,
+        marginRight: 5,
+        marginLeft: 5,
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 13,
         flexDirection: "column",
     },
+    menuItemActive:{
+        flex: 1,
+        height: 45,
+        marginRight: 5,
+        marginLeft: 5,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 13,
+        flexDirection: "column",
+        borderTopColor: "#1877F2",
+        borderTopWidth: 1
+    }
 
 });
--- Components/Pages/Home.js
+++ Components/Pages/Home.js
@@ -1,41 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Button,ScrollView, SafeAreaView, FlatList, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { View, StyleSheet, Button,ScrollView, SafeAreaView, FlatList, Text, TextInput, ActivityIndicator, TouchableOpacity, Image } from 'react-native';
 import axios from 'axios';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Moment from 'moment';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default function Home({ history }) {
     const [post, setPost] = useState([]);
-    useEffect(()=>{
-        axios.get("https://backend-api37.herokuapp.com/api/posts?maxResultCount=8")
-        .then(response => setPost(response.data.items))
-    },[])
+    const [loading, setLoading] = useState(false);
+    const [offset, setOffset] = useState(1);
+    const [isListEnd, setIsListEnd] = useState(false);
+    useEffect(() => {
+        getData()
+    }, [])
 
+    const getData = () => {
+        if (!loading && !isListEnd) {
+            setLoading(true);
+            axios.get("https://backend-api37.herokuapp.com/api/posts?maxResultCount=8&&skipCount=" + offset)
+                .then(response => {
+                    if (response.data.totalCount > 0) {
+                        setOffset(offset + 1);
+                        // After the response increasing the offset
+                        setPost([...post, ...response.data.items]);
+                        setLoading(false);
+                    } else {
+                        setIsListEnd(true);
+                        setLoading(false);
+                    }
+                })
+        }
+    };
     return (
         <View style={styles.container}>
             <View style={styles.extention}>
                 <View style={styles.extentionItem1}>
-                    <Text style={{color:"white"}}>Đăng ký tờ khai y tế</Text>
+                    <FontAwesome5 style={styles.icon} name={'file-medical'} size={25} />
+                    <Text style={styles.iconText}>Khai y tế</Text>
                 </View>
                 <View style={styles.extentionItem1}>
-                <Text style={{color:"white"}}>Đanh sách bác sĩ</Text>
+                    <FontAwesome5 style={styles.icon} name={'user-nurse'} size={25} />
+                    <Text style={styles.iconText}>Tìm kiếm bác sĩ</Text>
                 </View>
             </View>
             <View style={styles.extention}>
                 <View style={styles.extentionItem2}>
-                    <Text style={{color:"white"}}>Thống kê</Text>
+                    <FontAwesome5 style={styles.icon} name={'table'} size={25} />
+                    <Text style={styles.iconText}>Thống kê</Text>
                 </View>
                 <View style={styles.extentionItem2}>
-                    <Text style={{color:"white"}}>Quảng cáo</Text>
+                    <FontAwesome5 style={styles.icon} name={'ad'} size={25} />
+                    <Text style={styles.iconText}>Quảng cáo</Text>
                 </View>
             </View>
+
+            
             <View style={styles.contentTop}>
             { post.map((value, index)=>{
-                
                 Moment.locale('vi');
                 var dt =  value.createAt;
                 return(
-                    <View key={index} style={styles.item}> 
+                    <TouchableOpacity key={index} style={styles.item} onPress={() => {history.push("/detail?"+value.id)} }> 
                         <View style={styles.imgItem}>
                             {value.images.map((value, index) => {
                                 return <Image key={index} style={styles.img} source = {{ uri:"https://backend-api37.herokuapp.com/store-image/"+ value.path}} />
@@ -45,24 +69,40 @@ export default function Home({ history }) {
                             <Text style={styles.contentDate}>{Moment(dt).format('LL')}</Text>
                             <Text style={styles.contentTitle}>{value.title}</Text>
                         </View>
-                    </View>
+                    </TouchableOpacity>
                 )
             })}
+                <View style={{alignItems:"center"}}>
+                { loading===true ? <View><ActivityIndicator size="large" color="#0000ff"  /></View>  : <View></View> }
+                </View>
+                
+                <View style={{alignItems:"center"}}>
+                { isListEnd ? <Text>Đã hết</Text> : loading===false ? <Button title="Xem thêm" onPress={ getData }/> : <View></View> }
+                </View>
             </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
         alignItems: "center",
         justifyContent: "flex-start",
         width:"100%",
-        marginBottom: 50,
+        marginBottom: 10,
     },
     contentTop:{
         marginTop: 20,
+    },
+    icon: {
+        color: "white",
+        marginRight: 10,
+    },
+    iconText:{
+        color:"white",
+        fontSize: 16,
     },
     item: {
         flexDirection: "row",
@@ -76,9 +116,10 @@ const styles = StyleSheet.create({
         flex:1,
     },
     img: {
-        height: "100%",
-        width: "90%",
-        borderRadius: 10,
+        height: "90%",
+        width: "88%",
+        borderRadius: 5,
+        marginLeft: 5,
     },
     contentItem: {
         flex:3,
@@ -96,24 +137,23 @@ const styles = StyleSheet.create({
         flexDirection:"row",
         width:"100%",
         height: 70,
-        
-        
     },
     extentionItem1:{
         flex: 1,
         margin: 5,
-        backgroundColor:"#4444ef",
+        backgroundColor:"#1877F2",
         justifyContent: "center",
-        textAlign: "center",
         borderRadius: 15,
+        flexDirection: "row",
+        alignItems: "center"
     },
     extentionItem2:{
         flex: 1,
         margin: 5,
-        backgroundColor:"#0e950e",
+        backgroundColor:"#00A400",
         justifyContent: "center",
-        textAlign: "center",
         borderRadius: 15,
+        flexDirection: "row",
+        alignItems: "center"
     }
-
 });
